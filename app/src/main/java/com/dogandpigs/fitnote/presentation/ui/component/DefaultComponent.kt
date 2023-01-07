a9681de1295dd542d07ebc63a1c9b2a6f6ecdd6a
claude.d <claude.d@kakaomobility.com>
2023-01-07 14:33:20 +0900

    feature: CompleteButton DefaultComponent.kt에 통합, CloseButton 추가

--- app/src/main/java/com/dogandpigs/fitnote/presentation/ui/component/DefaultComponent.kt
+++ app/src/main/java/com/dogandpigs/fitnote/presentation/ui/component/DefaultComponent.kt
@@ -1,19 +1,56 @@
 package com.dogandpigs.fitnote.presentation.ui.component
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.dogandpigs.fitnote.R
+import com.dogandpigs.fitnote.presentation.base.FigmaPreview
 import com.dogandpigs.fitnote.presentation.ui.theme.BrandPrimary
+
+@Composable
+internal fun WidthSpacer(width: Dp) {
+    Spacer(modifier = Modifier.width(width))
+}
+
+@Composable
+internal fun HeightSpacer(height: Dp) {
+    Spacer(modifier = Modifier.height(height))
+}
+
+internal fun Modifier.defaultBorder(): Modifier = composed {
+    this.border(
+        width = 1.dp,
+        color = colorResource(id = R.color.border_gray),
+        shape = RoundedCornerShape(10.dp)
+    )
+}
+
+@Composable
+internal fun CloseButton(onClick: () -> Unit) {
+    IconButton(
+        modifier = Modifier.wrapContentWidth(),
+        onClick = onClick,
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Close, contentDescription = "Back"
+        )
+    }
+}
+
 
 @Composable
 internal fun CompleteButton(text: String, onClick: () -> Unit) {
@@ -29,12 +66,10 @@ internal fun CompleteButton(text: String, onClick: () -> Unit) {
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.Bottom
     ) {
-        // TODO: 버튼 공통화
         OutlinedButton(
             modifier = Modifier
                 .fillMaxWidth()
                 .height(buttonHeight)
-//                    .padding(paddingValues)
                 .background(Color.Transparent),
             onClick = onClick,
             shape = RoundedCornerShape(5.dp),
@@ -51,3 +86,14 @@ internal fun CompleteButton(text: String, onClick: () -> Unit) {
         }
     }
 }
+
+@FigmaPreview
+@Composable
+private fun PreviewDefaultBorderRow() {
+    Box(
+        modifier = Modifier
+            .padding(16.dp)
+            .defaultBorder()
+    )
+}
+
