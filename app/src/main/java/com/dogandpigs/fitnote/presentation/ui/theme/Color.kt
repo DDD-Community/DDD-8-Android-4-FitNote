ec9a68b066427455f33137011c9669e39609b21e
claude.d <claude.d@kakaomobility.com>
2023-01-13 23:06:20 +0900

    feature: DesignSystem Color 추가

--- app/src/main/java/com/dogandpigs/fitnote/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/dogandpigs/fitnote/presentation/ui/theme/Color.kt
@@ -1,5 +1,6 @@
 package com.dogandpigs.fitnote.presentation.ui.theme
 
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -11,15 +12,70 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 val BrandPrimary = Color(0xFF0085FF)
+val BrandSecondary = Color(0xFF2664F7)
+val BrandTertiary = Color(0xFF89EFD9)
+
+val SubPrimary = Color(0xFFBFE0FF)
+val SubSecondary = Color(0xFF8EDBFF)
+val SubTertiary = Color(0xFFC4F7EC)
+
+val Gradient1 = Brush.horizontalGradient(listOf(BrandPrimary, SubPrimary))
+val Gradient2 = Brush.horizontalGradient(listOf(BrandPrimary, BrandSecondary))
+val Gradient3 = Brush.horizontalGradient(listOf(BrandSecondary, SubSecondary))
 
 val Alert = Color(0xFFD92A2A)
 
+/**
+ * Background / Modal
+ */
 val GrayScaleWhite = Color(0xFFFFFFFF)
 
+/**
+ * Background / Border
+ */
 val GrayScaleLightGray1 = Color(0xFFF4F5F8)
+
+/**
+ * Background / Border / Hover
+ */
 val GrayScaleLightGray2 = Color(0xFFE2E4E9)
 
+/**
+ * Disable
+ */
+val GrayScaleMidGray1 = Color(0xFFC4C9D5)
+
+/**
+ * Information / Border
+ */
 val GrayScaleMidGray2 = Color(0xFF9199AB)
+
+/**
+ *
+ */
 val GrayScaleMidGray3 = Color(0xFF636D81)
 
+/**
+ *
+ */
+val GrayScaleDarkGray1 = Color(0xFF454C5B)
+
+/**
+ * Text
+ */
 val GrayScaleDarkGray2 = Color(0xFF212836)
+
+/**
+ * Text
+ */
+val GrayScaleBlack = Color(0xFF14161A)
+
+/**
+ *
+ */
+val GrayScaleDim1 = Color(0x3314161A)
+
+/**
+ *
+ */
+val GrayScaleDim2 = Color(0x8014161A)
