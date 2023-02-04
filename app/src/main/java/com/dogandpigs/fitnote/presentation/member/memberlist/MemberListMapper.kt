e47006253df1cadf327552f809af31e1ac0f08ab
claude.d <claude.d@kakaomobility.com>
2023-02-05 01:19:12 +0900

    feat: Member id 추가

--- app/src/main/java/com/dogandpigs/fitnote/presentation/member/memberlist/MemberListMapper.kt
+++ app/src/main/java/com/dogandpigs/fitnote/presentation/member/memberlist/MemberListMapper.kt
@@ -3,6 +3,6 @@ package com.dogandpigs.fitnote.presentation.member.memberlist
 import com.dogandpigs.fitnote.data.source.remote.model.Member
 
 internal fun Member.toPresentation(): MemberUiModel = MemberUiModel(
-    id = this.userId?.toIntOrNull() ?: 0,
+    id = this.id,
     userName = this.userName ?: "",
 )
