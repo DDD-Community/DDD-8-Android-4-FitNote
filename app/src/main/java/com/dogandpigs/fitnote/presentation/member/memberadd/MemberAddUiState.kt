5fe721f036be74041f88ebf8378ebd6060da22a2
claude.d <claude.d@kakaomobility.com>
2023-02-07 00:48:22 +0900

    feat - 회원 추가 성별 수정

--- app/src/main/java/com/dogandpigs/fitnote/presentation/member/memberadd/MemberAddUiState.kt
+++ app/src/main/java/com/dogandpigs/fitnote/presentation/member/memberadd/MemberAddUiState.kt
@@ -5,7 +5,14 @@ internal data class MemberAddUiState(
     val profileImgUrl: String = "",
     val height: Int = 0,
     val weight: Int = 0,
-    val gender: Int = 0,
+    val gender: Gender = Gender.MALE,
     val isAddSuccess: Boolean = false
 ) {
+    enum class Gender(
+        val value: Int,
+        val text: String,
+    ) {
+        MALE(1, "남성"),
+        FEMALE(2, "여성"),
+    }
 }
