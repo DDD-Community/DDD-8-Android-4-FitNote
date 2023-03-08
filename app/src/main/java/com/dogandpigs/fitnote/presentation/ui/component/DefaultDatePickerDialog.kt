971fdc1be2fc3426e52e22a8338afb636ece9c30
claude.d <claude.d@kakaomobility.com>
2023-03-09 01:45:20 +0900

    feat - DefaultDatePickerDialog 추가

---
+++ app/src/main/java/com/dogandpigs/fitnote/presentation/ui/component/DefaultDatePickerDialog.kt
@@ -0,0 +1,53 @@
+package com.dogandpigs.fitnote.presentation.ui.component
+
+import android.annotation.SuppressLint
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+
+@SuppressLint("UnrememberedMutableState")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+internal fun DefaultDatePickerDialog(
+    visible: Boolean,
+    onDismissRequest: () -> Unit,
+    onClickConfirmButton: (Long?) -> Unit,
+    dateMilliSeconds: Long,
+) {
+    val snackState = remember { SnackbarHostState() }
+    SnackbarHost(hostState = snackState, Modifier)
+
+    if (visible) {
+        val datePickerState = rememberDatePickerState(initialSelectedDateMillis = dateMilliSeconds)
+        DatePickerDialog(
+            onDismissRequest = onDismissRequest,
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        onClickConfirmButton(datePickerState.selectedDateMillis)
+                        onDismissRequest()
+                    },
+                ) {
+                    Text("OK")
+                }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = onDismissRequest
+                ) {
+                    Text("Cancel")
+                }
+            }
+        ) {
+            DatePicker(state = datePickerState)
+        }
+    }
+}
