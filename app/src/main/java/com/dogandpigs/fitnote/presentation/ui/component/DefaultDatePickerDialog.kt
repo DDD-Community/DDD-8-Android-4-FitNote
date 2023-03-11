84b879b7198039732239b059652cfdb7d7ddf4cd
claude.d <claude.d@kakaomobility.com>
2023-03-12 08:23:05 +0900

    feat - DefaultDatePickerDialog 색상 및 버튼 수정

--- app/src/main/java/com/dogandpigs/fitnote/presentation/ui/component/DefaultDatePickerDialog.kt
+++ app/src/main/java/com/dogandpigs/fitnote/presentation/ui/component/DefaultDatePickerDialog.kt
@@ -1,17 +1,22 @@
 package com.dogandpigs.fitnote.presentation.ui.component
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDefaults
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.unit.dp
+import com.dogandpigs.fitnote.presentation.ui.theme.BrandPrimary
+import com.dogandpigs.fitnote.presentation.ui.theme.GrayScaleWhite
 
 @SuppressLint("UnrememberedMutableState")
 @OptIn(ExperimentalMaterial3Api::class)
@@ -25,6 +30,8 @@ internal fun DefaultDatePickerDialog(
     val snackState = remember { SnackbarHostState() }
     SnackbarHost(hostState = snackState, Modifier)
 
+    val width = (LocalView.current.width.dp / 4) - 12.dp
+
     if (visible) {
         // TODO 시간 보정
         val datePickerState = rememberDatePickerState(
@@ -33,24 +40,43 @@ internal fun DefaultDatePickerDialog(
         DatePickerDialog(
             onDismissRequest = onDismissRequest,
             confirmButton = {
-                TextButton(
-                    onClick = {
+                DefaultPositiveButton(
+                    modifier = Modifier
+                        .width(width)
+                        .fillMaxHeight(),
+                    positiveText = "확인",
+                    onClickPositive = {
                         onClickConfirmButton(datePickerState.selectedDateMillis)
                         onDismissRequest()
                     },
-                ) {
-                    Text("OK")
-                }
+                )
             },
             dismissButton = {
-                TextButton(
-                    onClick = onDismissRequest
-                ) {
-                    Text("Cancel")
-                }
-            }
+                DefaultNegativeButton(
+                    modifier = Modifier
+                        .width(width)
+                        .fillMaxHeight(),
+                    negativeText = "취소",
+                    onClickNegative = onDismissRequest,
+                )
+            },
+            colors = DatePickerDefaults.colors(
+                containerColor = GrayScaleWhite,
+            )
         ) {
-            DatePicker(state = datePickerState)
+            DatePicker(
+                state = datePickerState,
+                colors = DatePickerDefaults.colors(
+                    containerColor = GrayScaleWhite,
+                    currentYearContentColor = BrandPrimary,
+                    selectedYearContentColor = GrayScaleWhite,
+                    selectedYearContainerColor = BrandPrimary,
+                    selectedDayContentColor = GrayScaleWhite,
+                    selectedDayContainerColor = BrandPrimary,
+                    todayContentColor = BrandPrimary,
+                    todayDateBorderColor = BrandPrimary,
+                ),
+            )
         }
     }
 }
